f2d577da6d7460f22beb29e30a40d8141359e0c1
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-08-11 00:30:54 +0600

    CRUD Operation Successfull

--- index.js
+++ index.js
@@ -50,6 +50,24 @@ async function run() {
 
     })
 
+    app.put('/users/:id', async(req, res)=>{
+        const id  = req.params.id;
+        const user = req.body;
+        console.log(id,user);
+        const filter = {_id: new ObjectId(id)}
+        const options = { upsert: true };
+        const updateUser = {
+            $set: {
+                  name: user.name,
+                  email: user.email
+            }
+        }
+
+        const result = await usersCollection.updateOne(filter,updateUser,options);
+        res.send(result);
+
+    })
+
    app.post('/users', async(req, res) =>{
      const user = req.body;
      console.log('new user', user);
